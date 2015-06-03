fac5bc0f8d77c54699dfd4fca011cf2a4deec638
digitalLumberjack <digitallumberjack@gmail.com>
2015-06-03 22:47:36 +0200

    corrected missing staging installation for openal and physfs, modified dependencies and install process of zeldasolarus

--- package/zeldasolarusdx/zeldasolarusdx.mk
+++ package/zeldasolarusdx/zeldasolarusdx.mk
@@ -7,7 +7,7 @@ ZELDASOLARUSDX_VERSION = 1.4.1
 ZELDASOLARUSDX_SOURCE = v$(ZELDASOLARUSDX_VERSION).tar.gz
 ZELDASOLARUSDX_SITE = https://github.com/christopho/solarus/archive
 ZELDASOLARUSDX_LICENCE = GPL
-ZELDASOLARUSDX_DEPENDENCIES = libpthread-stubs sdl2 sdl2_image sdl2_ttf openal vorbis-tools modplugtools lua physfs
+ZELDASOLARUSDX_DEPENDENCIES = libpthread-stubs sdl2 sdl2_image sdl2_ttf openal vorbis-tools modplugtools luajit physfs
 
 define ZELDASOLARUSDX_RPI_FIXUP.in
 	$(SED) 's|/opt/vc/include|$(STAGING_DIR)/usr/include|g' $(@D)/CMakeLists.txt
@@ -15,5 +15,14 @@ define ZELDASOLARUSDX_RPI_FIXUP.in
 endef
 
 ZELDASOLARUSDX_PRE_CONFIGURE_HOOKS += ZELDASOLARUSDX_RPI_FIXUP
+
+# Just copy the binary and lib, maybe we should copy more things after tests
+define ZELDASOLARUSDX_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/solarus_run \
+		$(TARGET_DIR)/usr/games/zeldasolarusdx/solarus_run
+	$(INSTALL) -D $(@D)/libsolarus.so \
+                $(TARGET_DIR)/usr/games/zeldasolarusdx/libsolarus.so
+
+endef
 
 $(eval $(cmake-package))
